ed8bfc526b44eb68372cb1185ff37c30ff889556
philipthedeveloper <philipowolabi79@gmail.com>
2025-05-16 21:56:28 +0100

    Add more styling to chatbot element

--- src/components/tabs/dashboard/bot/playground/ChatBot.tsx
+++ src/components/tabs/dashboard/bot/playground/ChatBot.tsx
@@ -1,17 +1,45 @@
 import { Button } from "@/components/form";
 import { useStore } from "@/hooks";
 import { Bot } from "@/interfaces";
-import { Settings2 } from "lucide-react";
+import { Send, Settings2 } from "lucide-react";
 
 const ChatBotHeader = ({ currentBot }: { currentBot: Bot }) => {
   return (
-    <div className="bg-gray-100 py-5 px-10">
+    <div className="bg-transparent p-4 pt-4 pb-0">
       <div className="flex items-center justify-between">
-        <p>{currentBot.name}</p>
+        <p className="tracking-tight font-dmsans text-white">
+          {currentBot.name}
+        </p>
         <Button variant="outline" size={"icon"} className="rounded-full">
           <Settings2 />
         </Button>
       </div>
+    </div>
+  );
+};
+
+const TextInput = () => {
+  return (
+    <div className="rounded-full w-full bg-white shadow-2xl min-h-15 flex items-center p-1">
+      <input
+        type="text"
+        className="flex-1 bg-transparent p-2 pl-6 bg-none outline-none border-none"
+        placeholder="Type and press [enter]"
+      />
+      <Button
+        className="bg-indigo-500 rounded-full h-13 w-13 aspect-square cursor-pointer hover:bg-indigo-500/70"
+        size={"icon"}
+      >
+        <Send className="!w-6 !h-6" />
+      </Button>
+    </div>
+  );
+};
+
+const Conversations = () => {
+  return (
+    <div className="bg-gray-100 flex-1 rounded-[28px] flex flex-col justify-end p-6">
+      <TextInput />
     </div>
   );
 };
@@ -24,8 +52,9 @@ export const ChatBot = () => {
       <div className="h-10">
         <p className="text-2xl font-dmsans tracking-tight">Chatbot</p>
       </div>
-      <div className=" bg-white shadow-[0px_4px_16px_0px_#0000001f] rounded-[12px] overflow-hidden w-full flex-1">
+      <div className="bg-white bg-linear-to-br from-indigo-500 to-sky-500 shadow-[0px_4px_16px_0px_#0000001f] rounded-4xl overflow-hidden w-full flex-1 p-1 flex flex-col gap-4">
         <ChatBotHeader currentBot={currentBot!} />
+        <Conversations />
       </div>
     </div>
   );
