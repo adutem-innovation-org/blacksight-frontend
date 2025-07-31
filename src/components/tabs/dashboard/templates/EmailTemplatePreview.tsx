c364be8df347c4e0d45a167774906f2fe10ac679
philipthedeveloper <philipowolabi79@gmail.com>
2025-07-31 14:49:23 +0100

    🪶[FEATURE]: Add email template previewer

---
+++ src/components/tabs/dashboard/templates/EmailTemplatePreview.tsx
@@ -0,0 +1,29 @@
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
+interface EmailTemplatePreviewProps {
+  isOpen: boolean;
+  onOpenChange: (value: boolean) => void;
+  preview: string;
+}
+
+export const EmailTemplatePreview = ({
+  isOpen,
+  onOpenChange,
+  preview,
+}: EmailTemplatePreviewProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      {/* <DialogTrigger>Open</DialogTrigger> */}
+      <DialogContent
+        onPointerDownOutside={(e) => e.preventDefault()}
+        onOpenAutoFocus={(e) => e.preventDefault()}
+        className="h-[85dvh] overflow-y-auto overflow-x-hidden !min-w-[90dvw] px-2 sm:px-6"
+      >
+        <div
+          className="w-full h-full py-8 overflow-hidden"
+          dangerouslySetInnerHTML={{ __html: preview }}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
