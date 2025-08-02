d5422e06e4a5bc6792d44445d917755731c6d3be
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-02 07:52:59 +0100

    ⬆️[UPDATE]: Restructure appointment entity

--- src/interfaces/appointment/store/appointment.ts
+++ src/interfaces/appointment/store/appointment.ts
@@ -1,13 +1,31 @@
 import { AppointmentStatus } from "@/enums";
+import { Bot } from "@/interfaces/bot";
+import { CalendarProvider } from "@/interfaces/calendar";
 
 export interface Appointment {
   _id: string;
+  summary?: string;
   businessId: string;
+  botId: string;
+  bot?: Bot; // Virtual property
+  providerId?: string;
+  provider?: CalendarProvider; // Virtual property
   conversationId: string;
   customerEmail: string;
-  appointmentDate: Date;
-  appointmentTime: Date;
-  meetingLink: string;
+  customerName: string;
+  customerPhone: string;
+  appointmentDateInCustomerTimezone: string;
+  appointmentTimeInCustomerTimezone: string;
+  appointmentDateInUTC: string;
+  appointmentTimeInUTC: string;
+  timezone: string;
+  dateTimeInCustomerTimezone: Date;
+  dateTimeInUTC: Date;
+  duration: number;
+  meetingLink?: string;
   status: AppointmentStatus;
-  dateTime?: Date; // Virtual property
+  confirmationEmailSent: boolean;
+  metadata: any;
+  scheduledByProvider: boolean;
+  createdAt: Date;
 }
