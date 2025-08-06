927c66ffaf77d3749326ff568374c42fb9c6f620
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-06 06:21:38 +0100

    💫[REFACTOR]: SourceWidget into a resuable component across tabs

--- src/components/tabs/dashboard/knowledge-base/AddKnowledgeBaseSourceWidget.tsx
+++ src/components/tabs/dashboard/knowledge-base/AddKnowledgeBaseSourceWidget.tsx
@@ -1,30 +1,8 @@
 import { kbSources, KBSourceType } from "@/constants";
-import { KnowledgeBaseSources, SideBarStateEnum } from "@/enums";
+import { SideBarStateEnum } from "@/enums";
 import { useStore } from "@/hooks";
 import { cn } from "@/lib/utils";
-
-type SourceWidgetProps = {
-  data: KBSourceType;
-  openForm: (data: KBSourceType) => void;
-};
-
-const SourceWidget = ({ openForm, data }: SourceWidgetProps) => {
-  const openKBForm = () => openForm(data);
-
-  const { label, iconClass } = data;
-
-  return (
-    <button
-      className="border border-gray-200 rounded-2xl px-4 py-3 pt-3.5 pr-6 flex flex-col items-start gap-3 cursor-pointer hover:bg-gray-100 bg-[#fbfbfb] w-full"
-      onClick={openKBForm}
-    >
-      <i className={cn(iconClass, "flex text-xl text-gray-500")}></i>
-      <span className="font-medium tracking-tighter leading-tight text-gray-800 text-sm">
-        {label}
-      </span>
-    </button>
-  );
-};
+import { SourceWidget } from "./SourceWidget";
 
 export const AddKnowledgeBaseSourceWidgets = ({
   openForm,
--- src/components/tabs/dashboard/knowledge-base/index.ts
+++ src/components/tabs/dashboard/knowledge-base/index.ts
@@ -2,3 +2,4 @@ export * from "./KnowledgeBaseTable";
 export * from "./KnowledgeBaseDetailsDrawer";
 export * from "./AddKnowledgeBaseForm";
 export * from "./AddKnowledgeBaseSourceWidget";
+export * from "./SourceWidget";
