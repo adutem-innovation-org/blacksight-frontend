bb2e2990cfc6cf9f96bce9ee29d5945a9b3d6dc5
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-07 05:07:40 +0100

    ⬆️[UPDATE]: Update start tracking component layout

--- src/components/tabs/dashboard/payment-tracker/StartTracking.tsx
+++ src/components/tabs/dashboard/payment-tracker/StartTracking.tsx
@@ -5,8 +5,13 @@ import { ChartNoAxesCombined } from "lucide-react";
 
 export const StartTracking = () => {
   return (
-    <div className="flex-1 w-full flex flex-col pt-16 sm:pt-12 items-center">
-      <DotLottieReact data={pulse} loop autoplay className="w-9/10 max-w-200" />
+    <div className="flex-1 w-full flex flex-col pt-16 sm:pt-12 items-center overflow-hidden">
+      <DotLottieReact
+        data={pulse}
+        loop
+        autoplay
+        className="max-w-200 max-h-100"
+      />
       <Button
         className="font-dmsans tracking-tight rounded-lg duration-500 hover:from-[#EE4266] hover:to-[#7C98DF]
  px-8 h-12 bg-gradient-to-r from-[#028CF3] to-[#3BCEAC]
