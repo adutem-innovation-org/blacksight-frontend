e3e877adee7391ad10243b8e285a631e07cea22a
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-10 20:42:10 +0100

    🪶[FEATURE]: Add reminder history tab

---
+++ src/components/tabs/dashboard/reminder/history/ReminderHistoryBreadCrumb.tsx
@@ -0,0 +1,30 @@
+import { SlashIcon } from "lucide-react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
+
+export const ReminderHistoryBreadCrumb = () => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/dashboard">Home</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <SlashIcon />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>Reminders</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
