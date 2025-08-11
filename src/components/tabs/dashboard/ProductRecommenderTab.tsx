c92659f9dac2dd819570456d831ccf9fbe2a6d78
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-11 10:04:44 +0100

    ⬆️[UPDATE]: Update product recommendation content container width

--- src/components/tabs/dashboard/ProductRecommenderTab.tsx
+++ src/components/tabs/dashboard/ProductRecommenderTab.tsx
@@ -167,7 +167,7 @@ export const ProductRecommenderTab = () => {
     <React.Fragment>
       <DashboardContent>
         <div className="flex-1 flex flex-col overflow-auto items-center h-full">
-          <div className="w-full max-w-6xl mt-4 md:mt-8 flex flex-col gap-6">
+          <div className="w-full max-w-7xl mt-4 md:mt-8 flex flex-col gap-6">
             <ProductRecommenderTabHeader />
             <UploadProductsWidget openForm={openCreateForm} />
             {productsSources && productsSources.length === 0 && (
