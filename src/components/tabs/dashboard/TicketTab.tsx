dc2cc6396e7649be82fa5de40fb74757f0912bb6
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-14 03:55:48 +0100

    🪶[FEATURE]: Add ticket table to ticket tab

--- src/components/tabs/dashboard/TicketTab.tsx
+++ src/components/tabs/dashboard/TicketTab.tsx
@@ -1,14 +1,27 @@
 import { DashboardContent } from "@/components/design";
-import { TicketTabHeader } from "./ticket";
+import { TicketsTable, TicketTabHeader } from "./ticket";
 import { useStore } from "@/hooks";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  deleteTicket,
   getAllTickets,
   getTicketAnalytics,
+  resetDeleteTicket,
   resetGetTicketAnalytics,
+  resetUpdateTicketStatus,
+  updateTicketStatus,
 } from "@/store";
 import { Loader } from "@/components/progress";
 import { motion } from "framer-motion";
+import { InfoBlock } from "@/components/InfoBlock";
+import { Button } from "@/components/form";
+import { RefreshCcw } from "lucide-react";
+import { resetDocumentElement } from "@/helpers";
+import { Ticket } from "@/interfaces";
+import toast from "react-hot-toast";
+import { ConfirmationDialog } from "@/components/popups";
+import { TicketStatus } from "@/enums";
+import { cn } from "@/lib/utils";
 
 export const TicketTab = () => {
   const { dispatch, getState } = useStore();
@@ -23,7 +36,116 @@ export const TicketTab = () => {
     ticketAnalyticsFetched,
     fetchTicketAnalyticsErrorMsg,
     ticketAnalytics,
+
+    // Delete ticket
+    deletingTicket,
+    ticketDeleted,
+    deleteTicketErrorMsg,
+
+    // Close ticket
+    updatingTicketStatus,
+    ticketStatusUpdated,
+    updateTicketStatusErrorMsg,
   } = getState("Ticket");
+
+  // Update ticket status
+  const [newStatus, setNewStatus] = useState<TicketStatus | null>(null);
+  const [ticketToUpdate, setTicketToUpdate] = useState<Ticket | null>();
+  const [updateDialogOpen, setUpdateDialogOpen] = useState(false);
+
+  const openUpdateDialog = () => setUpdateDialogOpen(true);
+  const closeUpdateDialog = () => setUpdateDialogOpen(false);
+
+  const triggerUpdate = (ticket: Ticket, status: TicketStatus) => {
+    setTicketToUpdate(ticket);
+    setNewStatus(status);
+    openUpdateDialog();
+  };
+
+  const endUpdateOperation = () => {
+    closeUpdateDialog();
+    setTicketToUpdate(null);
+    setNewStatus(null);
+    resetDocumentElement();
+  };
+
+  const confirmUpdateOperation = () => {
+    if (ticketToUpdate && newStatus) {
+      dispatch(
+        updateTicketStatus({
+          ticketId: ticketToUpdate._id,
+          status: newStatus,
+        })
+      );
+    }
+  };
+
+  // Update ticket status
+  useEffect(() => {
+    if (ticketStatusUpdated) {
+      toast.success("Updated");
+      // Get latest analytics
+      dispatch(getTicketAnalytics());
+      dispatch(resetUpdateTicketStatus());
+      // Close dialog, reset states and restore pointer event to document element
+      endUpdateOperation();
+    }
+  }, [ticketStatusUpdated]);
+
+  useEffect(() => {
+    if (updateTicketStatusErrorMsg) {
+      toast.error(updateTicketStatusErrorMsg);
+      // Reset states
+      dispatch(resetUpdateTicketStatus());
+    }
+  }, [updateTicketStatusErrorMsg]);
+
+  // Delete ticket
+  const [ticketToDelete, setTicketToDelete] = useState<Ticket | null>();
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+  const openDeleteDialog = () => setDeleteDialogOpen(true);
+  const closeDeleteDialog = () => setDeleteDialogOpen(false);
+
+  const triggerDeleteTicket = (ticket: Ticket) => {
+    setTicketToDelete(ticket);
+    openDeleteDialog();
+  };
+
+  const endDeleteOperation = () => {
+    closeDeleteDialog();
+    setTicketToDelete(null);
+    resetDocumentElement();
+  };
+
+  const confirmDeleteOperation = () => {
+    if (ticketToDelete) {
+      dispatch(deleteTicket(ticketToDelete._id));
+    }
+  };
+
+  // Delete ticket
+  useEffect(() => {
+    if (ticketDeleted) {
+      toast.success("Ticket deleted");
+      // Get the lastest analytics
+      dispatch(getTicketAnalytics());
+      dispatch(resetDeleteTicket());
+      // Close dialog, reset states and restore pointer event to document element
+      endDeleteOperation();
+    }
+  }, [ticketDeleted]);
+
+  useEffect(() => {
+    if (deleteTicketErrorMsg) {
+      toast.error(deleteTicketErrorMsg);
+      dispatch(resetDeleteTicket());
+    }
+  }, [deleteTicketErrorMsg]);
+
+  const refreshPage = () => {
+    dispatch(getAllTickets());
+  };
 
   useEffect(() => {
     if (!ticketAnalytics && !fetchingTicketAnalytics) {
@@ -53,16 +175,88 @@ export const TicketTab = () => {
   if (fetchingTicketAnalytics || fetchingAllTickets) return <Loader />;
 
   return (
-    <DashboardContent>
-      <div className="flex flex-col gap-4 h-full overflow-hidden">
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
-        >
-          <TicketTabHeader />
-        </motion.div>
-      </div>
-    </DashboardContent>
+    <React.Fragment>
+      <DashboardContent>
+        <div className="flex-1 flex flex-col overflow-y-auto items-center h-full no-scrollbar">
+          <div
+            className={cn(
+              "w-full max-w-[1680px] flex flex-col gap-6 flex-1 no-scrollbar"
+            )}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <TicketTabHeader />
+            </motion.div>
+
+            {fetchAllTicketsErrorMsg && (
+              <div className="mt-6 flex flex-col gap-4">
+                <InfoBlock variant={"error"}>
+                  {fetchAllTicketsErrorMsg}
+                </InfoBlock>
+                <Button
+                  className="font-dmsans tracking-tight rounded-lg duration-500 hover:from-[#EE4266] hover:to-[#7C98DF]
+ px-8 h-12 bg-gradient-to-r from-[#028CF3] to-[#3BCEAC] max-w-40 mx-auto"
+                  size={"sm"}
+                  onClick={refreshPage}
+                >
+                  Refresh
+                  <RefreshCcw />
+                </Button>
+              </div>
+            )}
+
+            {!fetchAllTicketsErrorMsg && tickets && (
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4, delay: 0.2 }}
+              >
+                <TicketsTable
+                  triggerUpdateStatus={triggerUpdate}
+                  triggerDeleteTicket={triggerDeleteTicket}
+                />
+              </motion.div>
+            )}
+
+            {/* Delete ticket */}
+            <ConfirmationDialog
+              cancelOperation={endDeleteOperation}
+              confirmOperation={confirmDeleteOperation}
+              isOpen={deleteDialogOpen}
+              loading={deletingTicket}
+              onOpenChange={endDeleteOperation}
+              title="Delete ticket"
+              description="This action cannot be undone. Are you sure you want to delete this ticket?"
+              cancelCtaText="Cancel"
+              confirmCtaText="Delete"
+            />
+
+            {/* Close reminder */}
+            <ConfirmationDialog
+              cancelOperation={endUpdateOperation}
+              confirmOperation={confirmUpdateOperation}
+              isOpen={updateDialogOpen}
+              loading={updatingTicketStatus}
+              onOpenChange={endUpdateOperation}
+              title={
+                newStatus === TicketStatus.CLOSED
+                  ? "Close ticket"
+                  : "Mark ticket as resolved"
+              }
+              description={`This action cannot be undone. Are you sure you want to ${
+                newStatus === TicketStatus.CLOSED
+                  ? "close this ticket?"
+                  : "mark this ticket as resolved?"
+              }`}
+              cancelCtaText="Cancel"
+              confirmCtaText="Continue"
+            />
+          </div>
+        </div>
+      </DashboardContent>
+    </React.Fragment>
   );
 };
---
+++ src/components/tabs/dashboard/ticket/TicketsTable.tsx
@@ -0,0 +1,182 @@
+import { cn } from "@/lib/utils";
+import { TicketTableHeader } from "./TableHeader";
+import { useStore } from "@/hooks";
+import { getAllTickets } from "@/store";
+import { useState } from "react";
+import {
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import { ticketsColumns } from "@/constants";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Ticket } from "@/interfaces";
+import { TicketStatus } from "@/enums";
+
+// Function to get responsive classes for each column
+const getColumnClasses = (columnId: string) => {
+  switch (columnId) {
+    case "agentName":
+      return "hidden md:table-cell"; // Always visible
+    case "agentStatus":
+      return "hidden xl:table-cell";
+    case "customerEmail":
+      return "";
+    case "customerName":
+      return "hidden md:table-cell";
+    case "status":
+      return "hidden lg:table-cell";
+    case "priority":
+      return "hidden lg:table-cell";
+    case "updatedAt":
+      return "hidden xl:table-cell"; // Hidden on mobile, visible from 692px+
+    case "actions":
+      return ""; // Always visible
+    default:
+      return "";
+  }
+};
+
+type TicketsTableProps = {
+  triggerUpdateStatus: (ticket: Ticket, status: TicketStatus) => void;
+  triggerDeleteTicket: (ticket: Ticket) => void;
+};
+
+export const TicketsTable = ({
+  triggerDeleteTicket,
+  triggerUpdateStatus,
+}: TicketsTableProps) => {
+  const { getState, dispatch } = useStore();
+  const { tickets } = getState("Ticket");
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>();
+
+  const onRefresh = () => {
+    dispatch(getAllTickets());
+  };
+
+  const table = useReactTable({
+    data: tickets || [],
+    meta: {
+      triggerDeleteTicket,
+      triggerUpdateStatus,
+    },
+    columns: ticketsColumns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <div
+      className={cn(
+        "grid grid-rows-auto sm:grid-rows-[80px_max-content] relative max-w-full overflow-x-hidden"
+      )}
+    >
+      <div className="max-w-full flex items-center sm:border-t">
+        <TicketTableHeader
+          onChange={(e: any) => {
+            table.getColumn("customerEmail")?.setFilterValue(e.target.value);
+          }}
+          value={
+            (table.getColumn("customerEmail")?.getFilterValue() as string) ?? ""
+          }
+          onRefresh={onRefresh}
+        />
+      </div>
+      <div className="md:border-t w-full overflow-x-auto">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow
+                key={headerGroup.id}
+                className="hover:bg-transparent border-b-gray-200/40 !border-b-2"
+              >
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className={cn(
+                        "py-4 px-2 hover:bg-gray-200 text-base font-dmsans font-medium tracking-tight whitespace-nowrap",
+                        getColumnClasses(header.id),
+                        {
+                          "!p-0":
+                            header.column.getCanFilter() ||
+                            header.column.getCanSort(),
+                          "hover:bg-transparent": header.id === "actions",
+                        }
+                      )}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="cursor-pointer hover:bg-gray-200/30"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      className={cn(
+                        "px-2 pl-4 py-3",
+                        getColumnClasses(cell.column.id)
+                      )}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={table.getAllColumns().length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
