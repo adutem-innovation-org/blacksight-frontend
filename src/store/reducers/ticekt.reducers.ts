828ace0a0e9394b3f3e59ca7855793d30b183ca2
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-14 01:09:55 +0100

    ✨[REDUCERS]: Implement state reset reducers for ticket service

---
+++ src/store/reducers/ticekt.reducers.ts
@@ -0,0 +1,37 @@
+import { TicketState } from "@/interfaces";
+
+export const resetDeleteTicketReducer = (state: TicketState) => {
+  state.deletingTicket = false;
+  state.ticketDeleted = false;
+  state.deleteTicketErrorMsg = "";
+};
+
+export const resetGetAllTicketsReducer = (state: TicketState) => {
+  state.fetchingAllTickets = false;
+  state.allTicketsFetched = false;
+  state.fetchAllTicketsErrorMsg = "";
+};
+
+export const resetGetTicketAnalyticsReducer = (state: TicketState) => {
+  state.fetchingTicketAnalytics = false;
+  state.ticketAnalyticsFetched = false;
+  state.fetchTicketAnalyticsErrorMsg = "";
+};
+
+export const resetReplyTicketReducer = (state: TicketState) => {
+  state.replyingTicket = false;
+  state.ticketReplied = false;
+  state.replyTicketErrorMsg = "";
+};
+
+export const resetUpdateTicketPriorityReducer = (state: TicketState) => {
+  state.updatingTicketPriority = false;
+  state.ticketPriorityUpdated = false;
+  state.updateTicketPriorityErrorMsg = "";
+};
+
+export const resetUpdateTicketStatusReducer = (state: TicketState) => {
+  state.updatingTicketStatus = false;
+  state.ticketStatusUpdated = false;
+  state.updateTicketStatusErrorMsg = "";
+};
